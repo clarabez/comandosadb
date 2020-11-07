d92478ef8c4d1957db64aabf1123e4788c59bdb4
Maria Clara dos Santos Bezerra <clarinhab@gmail.com>
2020-11-07 10:19:14 -0300

    Inserir script para função getinfo()

--- scripts/comandos.sh
+++ scripts/comandos.sh
@@ -29,3 +29,33 @@ input() # Esta função vai receber como parâmetro o que vc desejar passar como
 {
  adb shell input text "$1"
 }
+
+function getinfo () {
+
+        echo "============================== DEVICE RELEVANT INFO ==============================="
+
+        echo FINGERPRINT:
+
+        adb shell getprop ro.build.fingerprint
+
+        echo "SIM INFO : $(adb shell getprop gsm.sim.operator.alpha) | $(adb shell getprop gsm.sim.operator.numeric)"
+
+        echo "SIM FULL IMSI : $(adb shell service call iphonesubinfo 7 | awk -F "'" '{print $2}' | sed '1 d'| tr -d '\n' | tr -d '.' | tr -d ' ')"
+
+        echo "ro.CARRIER : $(adb shell getprop ro.carrier)"
+
+        echo "MODEL : $(adb shell getprop ro.product.model)"
+
+        echo "IMEI : $(adb shell service call iphonesubinfo 1 | awk -F "'" '{print $2}' | sed '1 d'| tr -d '\n' | tr -d '.' | tr -d ' ')"
+
+        echo "SERIAL : $(adb shell getprop ro.boot.serialno)"
+
+        echo "SKU : $(adb shell getprop ro.boot.hardware.sku)"
+
+        echo "HW : $(adb shell getprop ro.vendor.hw.revision)"
+
+        echo "Radio : $(adb shell getprop ro.vendor.hw.radio)"
+
+        echo "==================================================================================="
+
+}
